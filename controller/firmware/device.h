3947d6b0440095558e971ff79f28d43096cbe8cc
Jeff Gough <jeff@warrantyvoidifremoved.com>
2010-07-03 00:37:58 +0100

    header files

---
+++ controller/firmware/device.h
@@ -0,0 +1,14 @@
+#ifndef __DEVICE_H
+#define __DEVICE_H
+
+#include <msp430x261x.h>
+
+/* define PORT A registers */
+sfrw(PAREN, 0x0014);
+sfrw(PASEL, 0x003E);
+sfrw(PADIR, 0x003C);
+sfrw(PAOUT, 0x003A);
+sfrw(PAIN,  0x0038);
+
+#endif
+
---
+++ controller/firmware/wavegen.h
@@ -0,0 +1,6 @@
+#ifndef __WAVEGEN_H
+#define __WAVEGEN_H
+#define WAVETABLE_SZ 16
+void wavegen_init();
+
+#endif
