ca50327a769ff05562cd509b582a90a30a140da2
Johannes van der Horst <johannes.vanderhorst@gmail.com>
2010-12-21 16:38:12 +0000

    Adding files for image storage and management

---
+++ controller/firmware/image.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+/* 
+ * set up variables to point to image data
+ * TODO select different images
+ *
+ * Return 
+ * - w, h in pixels
+ * - pointer to image data
+ */
+
+void image_print(uint16_t *w, uint16_t *h, uint8_t * data);
