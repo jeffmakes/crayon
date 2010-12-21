1111af3d90b49804c854e7f2ad555e31ed547705
Johannes van der Horst <johannes.vanderhorst@gmail.com>
2010-12-21 13:09:10 +0000

    Changed ISR to use function pointer

--- controller/firmware/print.c
+++ controller/firmware/print.c
@@ -4,32 +4,23 @@
 #include "print.h"
 
 volatile printstate_t printstate;
+uint16_t (*fptr)(void) = NULL;
 
 void print_init()
 {
   printstate = PRINT_IDLE;
+  fptr  = NULL
 }
 
 /* Called by stepper interrupt */
 void print_process()
 {
   uint8_t i = 0;
-
-  if (printstate == PRINT_PRINTING)
+  /* only do this if fptr is defined */
+  if ((printstate == PRINT_PRINTING) && (fptr))
     {
-      for (i=0;i<K_NOZZLES;i++)	/* Fire all nozzles */
-	bk_data[i] = 1;
-
       printhead_period();
     }
-}
-
-void print_line()
-{
-  printstate = PRINT_PRINTING;
-  stepper_carriagepos(2500, 1500);
-  stepper_carriagepos(1000, 1500);
-  printstate = PRINT_IDLE;
 }
 
 void print_cleancycle()
@@ -39,4 +30,31 @@ void print_cleancycle()
   stepper_carriagepos(500, SPEED_SLOW);
 }
 
+/* line with all nozzles blazing */
+void print_line()
+{
+  fptr = &print_fireall;
+  printstate = PRINT_PRINTING;
+  stepper_carriagepos(2500, 1500);
+  stepper_carriagepos(1000, 1500);
+  printstate = PRINT_IDLE;
+  fptr = NULL;
+}
 
+
+/* set per step fire action here, head movement controlled above */
+
+/* Fire all nozzles */
+void print_fireall(void)
+{
+  /* load next pixel */
+  for (i=0;i<K_NOZZLES;i++) 
+    bk_data[i] = 1;
+}
+
+/*  
+ *  Prints a number of 1mm squares to test nozzles
+ */
+void print_nozzle_test(void)
+{
+}
