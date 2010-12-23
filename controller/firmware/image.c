75bbef4820a0aa6ad34874a24f41f69c233871bb
Robert Spanton <rspanton@zepler.net>
2010-12-23 00:39:24 +0000

    Perform the RLE decoding in image_getpixel in the MSP firmware

--- controller/firmware/image.c
+++ controller/firmware/image.c
@@ -3,17 +3,25 @@
  */
 #include "image.h"
 
-void image_select(uint16_t *w, uint16_t *h, volatile uint8_t ** data)
-{
-  *w = image_width;
-  *h = image_height;
-  *data = image_data;
-}
-
 /* first pixel is MSB */
 uint8_t image_getpixel(uint16_t x, uint16_t y)
 {
-  return (image_data[y][x/8] & (1 << (7 - x%8))) ? 1:0;
+  const uint8_t *pos = &image_data[ row_offsets[y] ];
+  uint16_t xpos = 0;
+
+  /* Work through the row until the requested position */
+  while(1) {
+    uint8_t runlength = (*pos) & 0x7f;
+
+    if( xpos + runlength > x )
+      /* Found the entry we want */
+      break;
+
+    xpos += runlength;
+    pos++;
+  }
+
+  return ((*pos) >> 7) & 1;
 }
 
 
--- controller/firmware/image.h
+++ controller/firmware/image.h
@@ -11,7 +11,6 @@
  * - pointer to image data
  */
 
-void image_select(uint16_t *w, uint16_t *h, volatile uint8_t ** data);
 uint8_t image_getpixel(uint16_t x, uint16_t y) ;
 
 #endif
