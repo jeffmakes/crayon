35507410e13f2aa1af6433d726c8aa3f90a3e230
Robert Spanton <rspanton@zepler.net>
2010-12-23 00:17:54 +0000

    Turn this situation into an OOP situation
    Removes a lot of the kludge

--- controller/images/img2c.py
+++ controller/images/img2c.py
@@ -8,21 +8,24 @@ if len(sys.argv) != 2:
 Usage: img2c.py <image>\n\n"""
     exit()
 
-header = open("image_data.h", "w")
-c_file = open("image_data.c", "w")
+class RLEFiles:
+    def __init__(self, img_fname):
+        self.img = Image.open(img_fname)
+        self.w, self.h = self.img.size
+        self.img = self.img.convert("RGB")
+        # 2 colours
+        self.img = self.img.quantize(2)
 
-f = Image.open(sys.argv[1])
-w,h = f.size
-# 2 colours
-f = f.convert("RGB")
-f = f.quantize(2)
+        self.header = open("image_data.h", "w")
+        self.c_file = open("image_data.c", "w")
+        self.n_bytes = 0
+        self.row_offsets = []
 
-numbytes = 0
-ncols = h/8
-if h%8:
-    ncols += 1
+        self._output_header()
+        self._output_c_file()
 
-header.write( """#ifndef __IMAGE_DATA_H
+    def _output_header(self):
+        self.header.write( """#ifndef __IMAGE_DATA_H
 #define __IMAGE_DATA_H
 #include <stdint.h>
 
@@ -33,50 +36,56 @@ extern const uint8_t image_data[];
 #endif	/* __IMAGE_DATA_H */
 """ )
 
-c_file.write( """const uint16_t image_width = %d;
+    def _output_c_file(self):
+        self._output_c_start()
+
+        self.c_file.write( """const uint8_t image_data[] = {\n""" )
+        for y in range(0,self.h):
+            self._output_c_row(y)
+        self.c_file.write( """};\n""" )
+
+
+    def _output_c_start(self):
+        self.c_file.write( """const uint16_t image_width = %d;
 const uint16_t image_height = %d;
+""" % (self.w, self.h) )
 
-const uint8_t image_data[] = {
-""" % (w, h) )
+    def _output_c_row(self, row):
+        self.runval = None
+        self.runlength = 0
+        self.row_offsets += [self.n_bytes]
 
-def rle_entry_out( f, length, val ):
-    assert length < 128
-    b = val << 7
-    b |= length
-    c_file.write( "0x%2.2x, " % b )
+        for x in range(self.w):
+            pixel = self.img.getpixel((x,row)) & 1
 
-for y in range(h):
-    runval = None
-    runlength = 0
+            if pixel == self.runval:
+                self.runlength += 1
 
-    for x in range(w):
-        pixel = f.getpixel((x,y)) & 1
+                if self.runlength == 127:
+                    "Saturated this entry -- move on"
+                    self._rle_entry_out()
+                    self.runlength = 0 
 
-        if pixel == runval:
-            runlength += 1
+            elif self.runval != None:
+                "Change in value..."
+                self._rle_entry_out()
+                self.runlength = 1
 
-            if runlength == 127:
-                "Saturated this entry -- move on"
-                rle_entry_out( c_file, runlength, runval )
-                runlength = 0 
+            self.runval = pixel
 
-        elif runval != None:
-            "Change in value..."
-            rle_entry_out( c_file, runlength, runval )
-            runlength = 1
+        if self.runlength:
+            "Output the remainder of this row"
+            self._rle_entry_out()
 
-        runval = pixel
+        self.c_file.write("\n");
 
-    if runlength:
-        "Output the remainder of this row"
-        rle_entry_out( c_file, runlength, runval )
+    def _rle_entry_out(self):
+        assert self.runlength < 128
+        b = self.runval << 7
+        b |= self.runlength
+        self.c_file.write( "0x%2.2x, " % b )
+        self.n_bytes += 1
 
-        runlength = 0
-
-    c_file.write("\n");
-
-c_file.write( "};\n" )
-c_file.close()
-header.close()
+rle = RLEFiles(sys.argv[1])
 
 
