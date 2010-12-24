c1a1efd9bdcecdf8c2ab20cc4280a1241dcdc381
Jeff Gough <jeff@warrantyvoidifremoved.com>
2010-12-24 04:01:27 +0000

    image data for peace logo. now printing well.

---
+++ controller/firmware/image_data.h
@@ -0,0 +1,11 @@
+#ifndef __IMAGE_DATA_H
+#define __IMAGE_DATA_H
+#include <stdint.h>
+
+#define IMAGE_N_BITS 8
+extern const uint16_t image_width;
+extern const uint16_t image_height;
+extern const uint8_t image_data[];
+const uint16_t row_offsets[682];
+
+#endif	/* __IMAGE_DATA_H */
